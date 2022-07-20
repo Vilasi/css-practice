111dd7ba9b39efa13e3e374ddc7e8c7fe1f76821
Vilasi <VilasiCoding@gmail.com>
2022-07-20 17:04:21 -0400

    Continued box model practice

---
+++ exercises/exercise12/logic.js
@@ -0,0 +1,12 @@
+// This script is just a joke and I'm saving it for future lols
+
+let string = '';
+
+string = prompt('enter the money exchanged');
+
+if (string == '') {
+  console.log('false');
+} else {
+  console.log('true');
+  alert('Dean = Big poopoo stinky head');
+}
