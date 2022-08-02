94bdf62bbc2aa3450591289f992917054f5a8ffd
Vilasi <VilasiCoding@gmail.com>
2022-08-01 23:42:27 -0400

    Background property practice and main index buttons

---
+++ app.js
@@ -0,0 +1,7 @@
+// onclick = "location.href='pageurl.html';";
+
+let googleFonts = document.getElementById('google-fonts');
+
+googleFonts.addEventListener('click', () => {
+  window.open('https://fonts.google.com/');
+});
