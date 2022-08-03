958fd89e7319c6b9cd29180bfc4b5cfb4757b12b
Vilasi <VilasiCoding@gmail.com>
2022-08-03 05:40:56 -0400

    Flexbox Axes Practice

--- app.js
+++ app.js
@@ -9,3 +9,14 @@ let cssTricks = document.getElementById('css-tricks');
 cssTricks.addEventListener('click', () => {
   window.open('https://css-tricks.com/guides/');
 });
+
+let mdnDocs = document.getElementById('mdn-flexbox');
+
+mdnDocs.addEventListener('click', () => {
+  window.open(
+    'https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox'
+  );
+});
+
+// Saving this for later reference
+console.log(document.querySelector('#google-fonts'));
