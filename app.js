97283247cba38a6bf5b9844bd550607f84ed2151
Vilasi <VilasiCoding@gmail.com>
2022-08-02 23:00:55 -0400

    justify-content and flex-direction practice

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ let googleFonts = document.getElementById('google-fonts');
 googleFonts.addEventListener('click', () => {
   window.open('https://fonts.google.com/');
 });
+
+let cssTricks = document.getElementById('css-tricks');
+
+cssTricks.addEventListener('click', () => {
+  window.open('https://css-tricks.com/guides/');
+});
